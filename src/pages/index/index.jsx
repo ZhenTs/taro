18ba195dafb6cbc1fd764416d4891029e4e23e30
刘祯 <v-liuzhen@sinooceanland.com>
2020-03-18 22:00:33 +0800

    完善

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,130 +1,130 @@
-import Taro from '@tarojs/taro'
-import { AtTabBar } from 'taro-ui'
-import { View, ScrollView } from '@tarojs/components'
-import { connect } from '@tarojs/redux'
+import Taro from "@tarojs/taro";
+import { AtTabBar } from "taro-ui";
+import { View, ScrollView } from "@tarojs/components";
+import { connect } from "@tarojs/redux";
 
-import ServiceTab from '../service/components/tab'
-import OrderTab from '../order/components/tab'
-import MineTab from '../mine/components/tab'
+import ServiceTab from "../service/components/tab";
+import OrderTab from "../order/components/tab";
+import MineTab from "../mine/components/tab";
 
-import './index.scss'
-import serviceImg from '../../images/tab/service.png'
-import serviceSelectedImg from '../../images/tab/service.selected.png'
-import orderImg from '../../images/tab/order.png'
-import orderSelectedImg from '../../images/tab/order.selected.png'
-import mineImg from '../../images/tab/mine.png'
-import mineSelectedImg from '../../images/tab/mine.selected.png'
+import "./index.scss";
+import serviceImg from "../../images/tab/service.png";
+import serviceSelectedImg from "../../images/tab/service.selected.png";
+import orderImg from "../../images/tab/order.png";
+import orderSelectedImg from "../../images/tab/order.selected.png";
+import mineImg from "../../images/tab/mine.png";
+import mineSelectedImg from "../../images/tab/mine.selected.png";
 
 const customerTabList = [
   {
-    title: '服务',
+    title: "服务",
     image: serviceImg,
-    selectedImage: serviceSelectedImg,
+    selectedImage: serviceSelectedImg
   },
   {
-    title: '订单',
+    title: "订单",
     image: orderImg,
-    selectedImage: orderSelectedImg,
+    selectedImage: orderSelectedImg
   },
   {
-    title: '我的',
+    title: "我的",
     image: mineImg,
-    selectedImage: mineSelectedImg,
-  },
-]
+    selectedImage: mineSelectedImg
+  }
+];
 
 const employeeTabList = [
   {
-    title: '订单',
+    title: "订单",
     image: orderImg,
-    selectedImage: orderSelectedImg,
+    selectedImage: orderSelectedImg
   },
   {
-    title: '我的',
+    title: "我的",
     image: mineImg,
-    selectedImage: mineSelectedImg,
-  },
-]
+    selectedImage: mineSelectedImg
+  }
+];
 
 @connect(({ common, service, order, mine, loading }) => ({
   common,
   service,
   order,
   mine,
-  loading: loading.models,
+  loading: loading.models
 }))
 export default class Index extends Taro.Component {
-  constructor () {
-    super(...arguments)
+  constructor() {
+    super(...arguments);
     this.state = {
       current: 0,
-      loginType: this.props.common.loginType || 'customer',
+      loginType: this.props.common.loginType || "customer"
+    };
+  }
+
+  componentDidMount() {
+    this.handleNavigationBarTitle(0);
+  }
+
+  handleNavigationBarTitle(value) {
+    const { loginType } = this.state;
+    if (loginType === "customer") {
+      Taro.setNavigationBarTitle({ title: customerTabList[value].title });
+    } else {
+      Taro.setNavigationBarTitle({ title: employeeTabList[value].title });
     }
   }
 
-  componentDidMount () {
-    this.handleNavigationBarTitle(0)
-  }
-
-  handleNavigationBarTitle (value) {
-    const { loginType } = this.state
-    if (loginType === 'customer') {
-      Taro.setNavigationBarTitle({ title: customerTabList[value].title })
+  handleClick(value) {
+    const { loginType } = this.state;
+    if (loginType === "customer") {
+      Taro.setNavigationBarTitle({ title: customerTabList[value].title });
     } else {
-      Taro.setNavigationBarTitle({ title: employeeTabList[value].title })
-    }
-  }
-
-  handleClick (value) {
-    const { loginType } = this.state
-    if (loginType === 'customer') {
-      Taro.setNavigationBarTitle({ title: customerTabList[value].title })
-    } else {
-      Taro.setNavigationBarTitle({ title: employeeTabList[value].title })
+      Taro.setNavigationBarTitle({ title: employeeTabList[value].title });
     }
     this.setState({
-      current: value,
-    })
+      current: value
+    });
   }
 
-  renderCurrent () {
-    let currentTab
-    const { current, loginType } = this.state
-    if (loginType === 'customer') {
+  renderCurrent() {
+    let currentTab;
+    const { current, loginType } = this.state;
+    if (loginType === "customer") {
       if (current === 0) {
-        currentTab = <ServiceTab/>
+        currentTab = <ServiceTab />;
       } else if (current === 1) {
-        currentTab = <OrderTab/>
+        currentTab = <OrderTab />;
       } else {
-        currentTab = <MineTab/>
+        currentTab = <MineTab />;
       }
     } else {
       if (current === 0) {
-        currentTab = <OrderTab/>
+        currentTab = <OrderTab />;
       } else {
-        currentTab = <MineTab/>
+        currentTab = <MineTab />;
       }
     }
-    return currentTab
+    return currentTab;
   }
 
-  render () {
-
+  render() {
     return (
-      <View className='index-page'>
-        <ScrollView className='container'>
-          {this.renderCurrent()}
-        </ScrollView>
+      <View className="index-page">
+        <ScrollView className='container'>{this.renderCurrent()}</ScrollView>
         <AtTabBar
           fixed
           iconSize={24}
           selectedColor='#CF0000'
-          tabList={this.state.loginType === 'customer' ? customerTabList : employeeTabList}
+          tabList={
+            this.state.loginType === "customer"
+              ? customerTabList
+              : employeeTabList
+          }
           onClick={this.handleClick.bind(this)}
           current={this.state.current}
         />
       </View>
-
-    )
+    );
   }
 }
